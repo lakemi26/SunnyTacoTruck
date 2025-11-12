b703ca7ffa7e31ca725f6b17f2b9f4325bb9a866
lakemi26 <lakemi@outlook.com.br>
2025-11-12 17:42:19 -0300

    Style: Background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+    <div className="flex min-h-screen items-center justify-center ">
       
     </div>
   );
